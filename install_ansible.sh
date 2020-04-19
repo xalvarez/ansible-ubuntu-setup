723b2d1bbaced12d018dd6506e1da001bd2b297e
Xavier Alvarez <xalvarez@users.noreply.github.com>
2020-04-19 13:30:07 +0200

    Upgrade to Ubuntu 20.04 (#80)
    * Install ansible by means of apt
    
    * Install fzf
    
    * Remove git ppa
    
    * Remove flatpak ppa
    
    * Remove KeePass ppa
    
    * Move 'docker' to the end of role 'dev'
    
    * Replace 'bionic' with dynamic ansible variable
    
    * Remove 'dash-to-dock' (currently broken)
    
    * Remove unused extension
    
    * Add Ubuntu 20.04 notice
    
    * Install JDK 8, 11 and 14
    
    * Update git aliases
    
    * Revert "Remove git ppa"
    
    * Revert "Remove KeePass ppa"
    
    * Remove docker-ce
    
    * Remove openjdk-14
    
    * Upgrade jetbrains toolbox

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 
-# Installs Ansible as described here: https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html#latest-releases-via-apt-ubuntu
-
-sudo apt update
-sudo apt install software-properties-common
-sudo apt-add-repository --yes --update ppa:ansible/ansible || exit 1
 sudo apt install ansible
